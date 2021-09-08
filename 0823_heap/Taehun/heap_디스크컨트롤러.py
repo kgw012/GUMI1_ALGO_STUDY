43ebaee62a7346f83bdf114aeb2523882895b8d1
강태훈 <xktmxkem@naver.com>
2021-09-09 00:19:47 +0900

    algorithm heap

--- 0823_heap/Taehun/heap_디스크컨트롤러.py
+++ 0823_heap/Taehun/heap_디스크컨트롤러.py
@@ -3,43 +3,51 @@ import heapq
 def solution(jobs):
     answer = 0
     heap = []
+    task = []
+    length = len(jobs)
+    time = 0
+    require_time = 0
     for y, x in jobs:
         heapq.heappush(heap, (y, x))
-    work = []
-    time = 0
-    length = len(heap)
-    task_time = 0
-    total_time = 0
+
     while(1):
-
-        if len(work) == 0 and len(heap) == 0:
+        # 만약 일과 heap에 둘다 썻다면 break
+        if len(task) == 0 and len(heap) == 0:
             break
+        # 만약 테스크는 종료했느느데 힙에 일이 남았으면
+        # 테스크에 일을 추가하고 대기한 시간만큼 총 시간에 더해준다
+        # 대기한 시간은 앞의 작업이 끝난시간에서 다음 작업의 요청시간의 차만큼 더해준다
+        if len(task) == 0 and len(heap) > 0:
+            temp = heapq.heappop(heap)
+            before_time = time
+            time += (temp[0] - before_time)
+            heapq.heappush(task, (temp[1], temp[0]))
+            continue
 
-        # 작업공간이 비엇다면 work에 작업시간만큼 추가한다
-        if len(work) == 0:
-            task = heapq.heappop(heap)
-            heapq.heappush(work, (task[1], task[0]))
-
-        # 요청시간에 도달하게 되면 work에 시간을 추가한다
-        if heap:
-           if heap[0][0] == time:
-                task = heapq.heappop(heap)
-                heapq.heappush(work, (task[1], task[0]))
-                continue
-
-        if work:
-            if work[0][0] == task_time:
-                out_task = heapq.heappop(work)
-                total_time += (time - out_task[1])
-                task_time = 0
-                continue
-            else:
-                task_time += 1
-                time += 1
+        elif len(task) > 0 and len(heap) > 0:
+            complete_task = heapq.heappop(task)
+            time += complete_task[0]
+            require_time += (time - complete_task[1])
+            # 힙이 있을 동안
+            while(heap):
+                # 현재 시간이 힙의 요청 시간보다 크거나 같은경우 추가한다 작업에, 단 작업량 순으로
+                if time >= heap[0][0]:
+                    temp = heapq.heappop(heap)
+                    heapq.heappush(task, (temp[1], temp[0]))
+                else:
+                    break
+        # 힙에서 다 추가한뒤 작업할 작업만 남으면
+        elif len(task) > 0 and len(heap) == 0:
+            while(task):
+                complete_task = heapq.heappop(task)
+                time += complete_task[0]
+                require_time += (time - complete_task[1])
 
 
+    answer = (require_time// length)
 
-    answer = (int(total_time / length))
+
+
 
 
     return answer
