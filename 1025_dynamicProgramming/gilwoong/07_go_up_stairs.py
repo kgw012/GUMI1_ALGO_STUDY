65dca40beca6f1ddd1d31265ccf1d7b82ed74184
kgw012 <kgw012@gmail.com>
2021-10-25 00:10:44 +0900

    gilwoong dp solving

--- 1025_dynamicProgramming/gilwoong/07_go_up_stairs.py
+++ 1025_dynamicProgramming/gilwoong/07_go_up_stairs.py
@@ -14,12 +14,12 @@ if __name__ == '__main__':
     dp2[0] = stairs[0]
     
     if n >= 2:
-        dp1[1] = stairs[0] + stairs[1]
-        dp2[1] = stairs[1]
+        dp1[1] = stairs[1]
+        dp2[1] = stairs[0] + stairs[1]
     
     for i in range(2, n):
-        dp1[i] = dp2[i - 1] + stairs[i]
-        dp2[i] = max(dp1[i - 2], dp2[i - 2]) + stairs[i]
+        dp1[i] = max(dp1[i - 2], dp2[i - 2]) + stairs[i]
+        dp2[i] = dp1[i - 1] + stairs[i]
     
     answer = max(dp1[n - 1], dp2[n - 1])
     print(answer)
--- 1025_dynamicProgramming/gilwoong/10_wine_tasting.py
+++ 1025_dynamicProgramming/gilwoong/10_wine_tasting.py
@@ -2,4 +2,37 @@
 # https://www.acmicpc.net/problem/2156
 
 if __name__ == '__main__':
+    n = int(input())
+    wines = []
+    for _ in range(n):
+        wines.append(int(input()))
+    
+    if n == 1:
+        answer = wines[0]
+        answer = wines[0]
+
+    elif n == 2:
+        answer = wines[0] + wines[1]
+    
+    else:
+        dp0 = [0] * n
+        dp1 = [0] * n
+        dp2 = [0] * n
+
+        dp0[0] = 0
+        dp1[0] = wines[0]
+        dp2[0] = 0
+
+        dp0[1] = wines[0]
+        dp1[1] = wines[1]
+        dp2[1] = wines[0] + wines[1]
+    
+        for i in range(2, n):
+            dp0[i] = max(dp0[i - 1], dp1[i - 1], dp2[i - 1])
+            dp1[i] = dp0[i - 1] + wines[i]
+            dp2[i] = dp1[i - 1] + wines[i]
+        
+        answer = max(dp0[n - 1], dp1[n - 1], dp2[n - 1])
+    
+    print(answer)
     
